01f27db222ce8553a7601b09300fccde19c11f17
NIRMALRAJ <80941732+nirmalrajs106@users.noreply.github.com>
2022-01-05 12:21:50 +0530

    Add files via upload

---
+++ Company 1-GoldmanSachs/12.Squares in Chess Board.java
@@ -0,0 +1,29 @@
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            Long N = Long.parseLong(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.squaresInChessBoard(N));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static Long squaresInChessBoard(Long N) {
+        // code here
+       if(N==1) return (long)1;
+        return (N*N)+squaresInChessBoard(N-1);
+    }
+};
