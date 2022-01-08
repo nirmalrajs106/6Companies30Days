e3c240fcc45c6e680f1b73c4aa1b2923608d8410
NIRMALRAJ <80941732+nirmalrajs106@users.noreply.github.com>
2022-01-08 22:01:12 +0530

    Add files via upload

---
+++ Company-2 Amazon/3.Ipl 2021 Match Day 2.java
@@ -0,0 +1,78 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main {
+    public static void main(String args[]) {
+        // taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // taking total count of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // taking total number of elements
+            int n = sc.nextInt();
+
+            // taking size of subArrays
+            int k = sc.nextInt();
+
+            // Declaring and Intializing an array of size n
+            int arr[] = new int[n];
+
+            // adding all the elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            // Calling the method max_of_subarrays of class solve
+            // and storing the result in an ArrayList
+            ArrayList<Integer> res = new Solution().max_of_subarrays(arr, n, k);
+
+            // printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution {
+    static ArrayList<Integer> max_of_subarrays(int arr[], int n, int k) {
+        // Your code here
+       TreeMap<Integer , Integer> map = new TreeMap<>();
+        
+        for(int i = 0; i< k-1 ; i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i] , map.get(arr[i]) + 1);
+            }
+            else{
+                map.put(arr[i] , 1);
+            }
+        }
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = k-1 ; i< n; i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i] , map.get(arr[i]) + 1);
+            }
+            else{
+                map.put(arr[i] , 1);
+            }
+            list.add(map.lastKey());
+            
+            int num = arr[i - k +1];
+            
+            if(map.get(num) > 1){
+                map.put(num , map.get(num) -1);
+            }
+            else{
+                map.remove(num);
+            }
+        }    
+        
+        return list;
+    }
+}
