06495f7d73ba549ca4b116f29653333f4f633758
NIRMALRAJ <80941732+nirmalrajs106@users.noreply.github.com>
2022-01-10 19:56:23 +0530

    Create 8.Count ways to nth stair.java

---
+++ Company-2 Amazon/8.Count ways to nth stair.java
@@ -0,0 +1,42 @@
+
+
+class GFG {
+	public static void main (String[] args) {
+		
+		//taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking total testcases
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+		    
+		    //taking total number of stairs
+		    int m = sc.nextInt();
+		    
+		    //creating an object of class DynamicProgramming
+		    Solution obj = new Solution();
+		    
+		    //calling method of class countWays()
+		    //of class DynamicProgramming
+		    System.out.println(obj.countWays(m));
+		    
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    Long countWays(int m)
+    {
+        // your code here
+         long ans=(m/2)+1;
+         return ans;
+    }    
+}
